d88d47c31627a7c72ffd3159220b5db2833452c6
Luca Meyer <lmeyer@Lucas-MacBook-Pro.local>
2021-06-11 15:53:25 +0200

    Add react-native-svg

--- App.tsx
+++ App.tsx
@@ -8,108 +8,49 @@
  * @format
  */
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import Svg, {Rect, Defs, LinearGradient, Stop} from 'react-native-svg';
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>SVG Test</Text>
+      <Svg>
+        <Defs>
+          <LinearGradient id="gradOuter" x1="0" y1="0" x2="1" y2="0">
+            <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
+            <Stop offset="1" stopColor="red" stopOpacity="1" />
+          </LinearGradient>
+        </Defs>
+        <Rect x="0" y="0" width="50" height="100" fill="blue" />
+        <Rect x="60" y="0" width="50" height="100" fill="url(#gradOuter)" />
+        <Svg>
+          <Defs>
+            <LinearGradient id="gradInner" x1="0" y1="0" x2="1" y2="0">
+              <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
+              <Stop offset="1" stopColor="red" stopOpacity="1" />
+            </LinearGradient>
+          </Defs>
+          <Rect x="120" y="0" width="50" height="100" fill="blue" />
+          <Rect x="180" y="0" width="50" height="100" fill="url(#gradInner)" />
+        </Svg>
+        <Rect x="240" y="0" width="50" height="100" fill="blue" />
+        <Rect x="300" y="0" width="50" height="100" fill="url(#gradOuter)" />
+      </Svg>
+    </SafeAreaView>
+  );
+};
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({});
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
-
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
-
- export default App;
+export default App;
