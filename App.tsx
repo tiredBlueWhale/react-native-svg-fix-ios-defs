4f3f990b03e95f28634e14ff00c635402b8216f6
Kristof Gilicze <kristof.gilicze@horizon-alpha.com>
2021-06-14 11:43:40 +0200

    reposition elements for clarity

--- App.tsx
+++ App.tsx
@@ -19,7 +19,13 @@ import {
   View,
 } from 'react-native';
 
-import Svg, {Rect, Defs, LinearGradient, Stop} from 'react-native-svg';
+import Svg, {
+  Rect,
+  Defs,
+  LinearGradient,
+  Stop,
+  Text as SvgText,
+} from 'react-native-svg';
 
 const App = () => {
   return (
@@ -32,8 +38,17 @@ const App = () => {
             <Stop offset="1" stopColor="red" stopOpacity="1" />
           </LinearGradient>
         </Defs>
-        <Rect x="0" y="0" width="50" height="100" fill="blue" />
-        <Rect x="60" y="0" width="50" height="100" fill="url(#gradOuter)" />
+        <SvgText
+          fill="purple"
+          stroke="purple"
+          fontSize="20"
+          fontWeight="bold"
+          x="0"
+          y="20">
+          SVG Outer
+        </SvgText>
+        <Rect x="0" y="30" width="50" height="100" fill="blue" />
+        <Rect x="60" y="30" width="50" height="100" fill="url(#gradOuter)" />
         <Svg>
           <Defs>
             <LinearGradient id="gradInner" x1="0" y1="0" x2="1" y2="0">
@@ -41,11 +56,43 @@ const App = () => {
               <Stop offset="1" stopColor="red" stopOpacity="1" />
             </LinearGradient>
           </Defs>
-          <Rect x="120" y="0" width="50" height="100" fill="blue" />
-          <Rect x="180" y="0" width="50" height="100" fill="url(#gradInner)" />
+          <SvgText
+            fill="purple"
+            stroke="purple"
+            fontSize="20"
+            fontWeight="bold"
+            x="150"
+            y="20">
+            SVG Inner
+          </SvgText>
+          <Rect
+            x="150"
+            y="30"
+            width="50"
+            height="100"
+            fill="blue"
+            stroke="black"
+            strokeWidth="4"
+          />
+          <Rect
+            x="210"
+            y="30"
+            width="50"
+            height="100"
+            fill="url(#gradInner)"
+            stroke="black"
+            strokeWidth="4"
+          />
+          <Rect
+            x="270"
+            y="30"
+            width="50"
+            height="100"
+            fill="url(#gradOuter)"
+            stroke="black"
+            strokeWidth="4"
+          />
         </Svg>
-        <Rect x="240" y="0" width="50" height="100" fill="blue" />
-        <Rect x="300" y="0" width="50" height="100" fill="url(#gradOuter)" />
       </Svg>
     </SafeAreaView>
   );
