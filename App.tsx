534a1fca294833770f66c6edb5739f3b96453c9a
Luca Meyer <lmeyer@Lucas-MacBook-Pro.local>
2021-06-17 09:19:27 +0200

    Change ui to #1437 of react-native-svg

--- App.tsx
+++ App.tsx
@@ -26,74 +26,62 @@ import Svg, {
   LinearGradient,
   Stop,
   Text as SvgText,
+  RadialGradient,
+  Ellipse,
+  Circle,
+  G,
 } from 'react-native-svg';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={styles.headerStyle}>SVG Test</Text>
-      <Svg>
+      <Svg width={150} height={50}>
         <Defs>
-          <LinearGradient id="gradOuter" x1="0" y1="0" x2="1" y2="0">
-            <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
-            <Stop offset="1" stopColor="red" stopOpacity="1" />
+          <LinearGradient
+            id="grad1"
+            x1={'0%'}
+            y1={'0%'}
+            x2={'100%'}
+            y2={'100%'}>
+            <Stop offset={0} stopColor="red" />
+            <Stop offset={1} stopColor="blue" />
           </LinearGradient>
         </Defs>
-        <SvgText
-          fill="purple"
-          stroke="purple"
-          fontSize="20"
-          fontWeight="bold"
-          x="0"
-          y="20">
-          SVG Outer
-        </SvgText>
-        <Rect x="0" y="30" width="50" height="100" fill="blue" />
-        <Rect x="60" y="30" width="50" height="100" fill="url(#gradOuter)" />
-        <Svg>
-          <Defs>
-            <LinearGradient id="gradInner" x1="0" y1="0" x2="1" y2="0">
-              <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
-              <Stop offset="1" stopColor="red" stopOpacity="1" />
-            </LinearGradient>
-          </Defs>
-          <SvgText
-            fill="white"
-            stroke="black"
-            fontSize="20"
-            fontWeight="bold"
-            x="150"
-            y="20">
-            SVG Inner
-          </SvgText>
-          <Rect
-            x="150"
-            y="30"
-            width="50"
-            height="100"
-            fill="blue"
-            stroke="black"
-            strokeWidth="4"
-          />
-          <Rect
-            x="210"
-            y="30"
-            width="50"
-            height="100"
-            fill="url(#gradInner)"
-            stroke="black"
-            strokeWidth="4"
-          />
-          <Rect
-            x="270"
-            y="30"
-            width="50"
-            height="100"
-            fill="url(#gradOuter)"
-            stroke="black"
-            strokeWidth="4"
-          />
-        </Svg>
+        <Rect x={0} y={0} width={150} height={50} fill={'pink'} />
+        {/* Renders gradient correctly*/}
+        <Circle cx={25} cy={25} r={25} fill={'url(#grad1)'} stroke="black" />
+
+        <G x={50} y={0}>
+          <Svg width={50} height={50}>
+            {/* Renders red BG correctly*/}
+            <Circle cx={25} cy={25} r={25} fill={'red'} stroke="black" />
+          </Svg>
+        </G>
+
+        <G x={100} y={0}>
+          <Svg width={50} height={50}>
+            <Defs>
+              <LinearGradient
+                id="grad2"
+                x1={'0%'}
+                y1={'0%'}
+                x2={'100%'}
+                y2={'100%'}>
+                <Stop offset={0} stopColor="red" />
+                <Stop offset={1} stopColor="blue" />
+              </LinearGradient>
+            </Defs>
+            {/* Does not render gradient, fill is transparent on iOS*/}
+            <Circle
+              cx={25}
+              cy={25}
+              r={25}
+              fill={'url(#grad2)'}
+              strokeWidth={1}
+              stroke="black"
+            />
+          </Svg>
+        </G>
       </Svg>
     </SafeAreaView>
   );
